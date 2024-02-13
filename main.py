d526b940c2ec8d86f83b9073703c5c91b27b1fdd
Awwab <mawwabkhank2006@gmail.com>
2024-02-13 12:36:10 +0500

    dynamic data passed to template

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    info = {
+        'name': 'M. Awwab Khan',
+        'designation': 'Machine Learning Scientist'
+    }
+    return render_template('index.html', info=info)
 
 if __name__ == '__main__':
     app.run(debug=True)
